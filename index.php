dd545b450961b732025e6f4f66df130616939752
panteLx <sebi.stahl1811@live.com>
2018-11-27 00:18:37 +0100

    test

--- index.php
+++ index.php
@@ -3,6 +3,5 @@
 	<body>
 <h1>Du wirst weitergeleitet.</h1>
 		<p>Falls dein Browser keine automatische Weiterleitung unterstuetzt, <a href="https://anonymebeichten.de/c.php?c=index" >klicke hier</a>!</p>
-
 	</body>
 </html>
